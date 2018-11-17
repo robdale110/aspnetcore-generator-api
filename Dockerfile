bcd451d409d6c20ccbc0bd87b29119b1f63b1e7d
Rob Dale <rob@robdale.me.uk>
2018-11-17 11:24:48 +0000

    Added new test

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+# build stage
+FROM microsoft/dotnet:2.1-sdk AS build-env
+
+WORKDIR /generator
+
+# restore
+COPY api/api.csproj ./api/
+RUN dotnet restore api/api.csproj
+COPY tests/tests.csproj ./tests/
+RUN dotnet restore tests/tests.csproj
+
+# Allows seeing what files are being copied into image - doesnt work if cached
+#RUN ls -alR
+
+# copy src
+COPY . .
+
+# Can also run the following command to list files and directories copied into image
+# docker build -t testing .
+# docker run --rm testing ls -alR
+
+# test
+RUN dotnet test tests/tests.csproj
+
+# publish
+RUN dotnet publish api/api.csproj -o /publish
+
+# runtime stage
+FROM microsoft/dotnet:2.1-aspnetcore-runtime
+COPY --from=build-env /publish /publish
+WORKDIR /publish
+ENTRYPOINT ["dotnet", "api.dll"]
---
+++ tests/RangeTests.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using api.Controllers;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CountShouldControlNumberOfResults()
+        {
+            var range = new Range { Count = 3};
+
+            var generated = range.Of(() => "");
+
+            Assert.Equal(3, generated.Count());
+        }
+
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new Range { Count = 3,  Sort = true };
+            var values = new[] { "a", "c", "b" };
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+
+            Assert.Equals(new[] { "a", "b", "c" }, generated.ToArray());
+        }
+    }
+}
